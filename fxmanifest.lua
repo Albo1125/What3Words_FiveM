fbe8f01786401e427620a27aed3ae85faaebcfd3
Albo1125 <13710765+Albo1125@users.noreply.github.com>
2022-05-10 20:07:29 +0200

    Update fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,6 @@ games { 'gta5' }
 author 'Albo1125'
 url 'https://github.com/Albo1125/What3Words_FiveM'
 
-files 'words.txt'
+file 'words.txt'
 server_script 'sv_What3Words.lua'
 client_script 'What3Words.net.dll'
